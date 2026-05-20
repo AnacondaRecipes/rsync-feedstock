b8646aac68d5f6a83d3f9f2743ab9d781a7910d7
vkhomits <khomitsvalia@gmail.com>
2026-05-20 16:52:16 +0300

    Add flag disable mknodat

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,11 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 if [[ "${target_platform}" == "osx-arm64" ]]; then
     EXTRA_CONFIGURE_ARGS="--disable-simd"
 fi
+# mknodat not declared in macOS SDK 12.1 headers with clang 20
+if [[ "${target_platform}" == osx-* ]]; then
+    export ac_cv_func_mknodat=no
+fi
+
 ./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-}
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
